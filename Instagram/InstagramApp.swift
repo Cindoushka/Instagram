0983be10a04b032137d4a7d4c70e7206360af132
Cindoushka <cindie-baj@live.fr>
2022-07-11 13:44:51 +0200

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Cindy Bajoni on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
